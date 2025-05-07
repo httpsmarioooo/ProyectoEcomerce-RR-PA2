195e510bd349ee836629b84d9ff56e4bf402fbd5
Carlos Mario <carlosmariobig14@gmail.com>
2025-05-07 16:56:32 -0500

    Refactorizar la visualización y el estilo del producto; mejorar el diseño y las interacciones de los botones

--- structure/productos.js
+++ structure/productos.js
@@ -1,21 +1,21 @@
 //Crear Card
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     fetch('productos.json')
         .then(response => response.json())
         .then(productos => {
             const container = document.getElementById('productos-container');//ID contenedor productos---//& Verificar
-            
+
             productos.forEach(producto => {
                 const card = document.createElement('div');
                 card.id = `card-${producto.id}`; // Id de producto
                 card.className = 'col-12 col-sm-6 col-md-4 d-flex justify-content-center p-0';
                 card.innerHTML = `
-<div class="card h-100 custom-card categoria-${producto.categoria} nivel-${producto.nivel} edad-${producto.edad}">
+                <div class="card h-100 custom-card categoria-${producto.categoria} nivel-${producto.nivel} edad-${producto.edad}">
                         <img src="${producto.imagen}" class="imagen-card" alt="${producto.titulo}">
                         <div class="card-body">
                             <h5 class="card-title">${producto.titulo}</h5>
                             <h3 class="card-price">Precio $ ${producto.precio.toLocaleString()}</h3>
-                            <a href="#" class="btn btn-primary">Comprar</a>
+                            <button class="btn bt-productos"><strong>Comprar</strong></button>
                         </div>
                     </div>
                 `;
