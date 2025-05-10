f1f4356069fa2240261b4ec6a7b002457b7c53df
Arkanhell98 <gabopc98@gmail.com>
2025-05-10 18:31:47 -0500

    Actualizacion admin y productos script para tomar productos del formulario

--- structure/productos.js
+++ structure/productos.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(response => response.json())
         .then(productos => {
             const container = document.getElementById('productos-container');//ID contenedor productos---//& Verificar
-            
+
             productos.forEach(producto => {
                 const card = document.createElement('div');
                 card.id = `card-${producto.id}`; // Id de producto
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         </div>
                         </div>
                         `;
-                        container.appendChild(card);//Agregar card al contenedor
+                container.appendChild(card);//Agregar card al contenedor
             });
         });
     }
