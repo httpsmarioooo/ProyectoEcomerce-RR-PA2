9906fcf62ad914d0d69c9f6ad50941effe1acf94
Arkanhell98 <gabopc98@gmail.com>
2025-05-12 15:13:12 -0500

    JS registro y login

--- js/register.js
+++ js/register.js
@@ -5,18 +5,33 @@ formulario.addEventListener('submit',(e)=>{
     const nombre = document.querySelector("#nombre").value;
     const correo = document.querySelector("#correo").value;
     const contrasena = document.querySelector("#contrasena").value;
+    const confirmar = document.querySelector("#confirmar").value
 
+    // Validacion de los campos
+    if (!usuario || !nombre || !correo || !contrasena || !confirmar) {
+        alert("Completa todos los campos.");
+        return;
+    }
+    
+    // Confirmar contraseñas
+    if(confirmar !== contrasena){
+        alert("Las contraseñas no coinciden");
+        return;
+    }
+
+    //Verificacion del correo para el usuario
     const listaUsuarios = JSON.parse(localStorage.getItem('listaUsuarios')) || [];
     const usuarioCreado = listaUsuarios.find(user => user.email === correo)
     if(usuarioCreado){
-        return alert("El usuario ya está registrado")
+        return alert("El usuario ya está registrado");
     }
-
+    
+    //Agregar a la lista y al localStorage
     listaUsuarios.push({
         usuario: usuario, 
         nombre: nombre,
         correo: correo,
-        constraseña:contrasena
+        constrasena:contrasena
     })
 
     localStorage.setItem('listaUsuarios',JSON.stringify(listaUsuarios))
