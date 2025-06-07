00eedbf35ac01ff8c43f95e4944e863f3a829ef9
Arkanhell98 <gabopc98@gmail.com>
2025-06-07 15:37:29 -0500

    Correción rutas imagenes admin

--- js/admin.js
+++ js/admin.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const precio = parseFloat(document.getElementById('precio').value);
         const categoria = document.getElementById('categoria').value;
         const nivel = document.getElementById('nivel').value;
-        const edadRecomendada = document.getElementById('edad').value;
+        const edad = document.getElementById('edad').value;
         const imagen = document.getElementById('imagen');
         const archivo = imagen.files[0];
 
@@ -23,74 +23,70 @@ document.addEventListener('DOMContentLoaded', function () {
             alert("Por favor, sube una imagen.");
             return;
         }
-
-        const data = {
-            titulo,
-            descripcion,
-            precio,
-            categoria,
-            nivel,
-            edadRecomendada,
-        };
-
-        fetch("http://localhost:8080/productos/guardarNuevoProducto", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(data)
-        })
-        .then(res => res.text())
-        .then(response => {
-            console.log("Respuesta del servidor:", response);
-            alert("Producto agregado correctamente.");
-            agregarForm.reset();
-        })
-        .catch(error => {
-            console.error("Error en la solicitud:", error);
+                const data = {
+                titulo: titulo,
+                descripcion: descripcion,
+                precio: precio,
+                categoria: categoria,
+                nivel: nivel,
+                edadRecomendada: edad,
+                imagenUrl: `/assets/imgProductos/${nivel}/${archivo.name}`
+            };
+            fetch("http://localhost:8080/productos/guardarNuevoProducto", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(data)
+            })
+            .then(res => res.text())
+            .then(response => {
+                console.log("Respuesta del servidor:", response);
+                alert("Producto agregado correctamente.");
+                agregarForm.reset();
+            })
+            .catch(error => {
+                console.error("Error en la solicitud:", error);
+            });
         });
     });
 
-    
     document.getElementById('btn-editarBD').addEventListener('click', function (event) {
         event.preventDefault();
-
-         if (!editarForm.checkValidity()) {
-        editarForm.reportValidity(); // Muestra los errores en pantalla
-        return;
-    }
-        const id = document.getElementById('id').value.trim();
+        const id = document.getElementById('id').value;
         const titulo = document.getElementById('tituloEdit').value;
         const descripcion = document.getElementById('descripcionEdit').value;
         const precio = parseFloat(document.getElementById('precioEdit').value);
         const categoria = document.getElementById('categoriaEdit').value;
         const nivel = document.getElementById('nivelEdit').value;
-        const edadRecomendada = document.getElementById('edadEdit').value;
+        const edad = document.getElementById('edadEdit').value;
         const imagen = document.getElementById('imagenEdit');
         const archivo = imagen.files[0];
-        
-        const data = {
-             titulo,
-             descripcion,
-             precio,  
-             categoria,
-             nivel,
-             edadRecomendada
-        };
 
-        console.log(data)
-
-        fetch(`http://localhost:8080/productos/editar/${id}`, {
-            method: "PUT",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(data)
-        })
-        .then(res => res.text())
-        .then(response => {
-            console.log("Respuesta del servidor:", response);
-            alert("Producto actualizado correctamente.");
-            editarForm.reset();
-        })
-        .catch(error => {
-            console.error("Error en la solicitud:", error);
+        if (!archivo) {
+            alert("Por favor, sube una imagen.");
+            return;
+        }
+                const data = {
+                titulo: titulo,
+                descripcion: descripcion,
+                precio: precio,
+                categoria: categoria,
+                nivel: nivel,
+                edadRecomendada: edad,
+                imagenUrl: `/assets/imgProductos/${nivel}/${archivo.name}`
+            };
+            fetch(`http://localhost:8080/productos/editar/${id}`, {
+                method: "PUT",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(data)
+            })
+            .then(res => res.text())
+            .then(response => {
+                console.log("Respuesta del servidor:", response);
+                alert("Producto actualizado correctamente.");
+                agregarForm.reset();
+            })
+            .catch(error => {
+                console.error("Error en la solicitud:", error);
         });
     });
 
@@ -109,6 +105,5 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .catch(error => {
             console.error("Error en la solicitud:", error);
-        });
     });
 });
